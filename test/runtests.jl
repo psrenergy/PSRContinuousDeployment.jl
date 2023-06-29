249bccb744627f473dc006a041bab2d06d7c315c
raphasampaio <rsampaio@psr-inc.com>
2023-06-29 11:33:12 -0300

    Updated runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,25 +2,34 @@ using Test
 using PSRContinuousDeployment
 
 function testall()
-    configuration = PSRContinuousDeployment.Configuration("PSRClustering", raw"D:\development\psrclustering\PSRClustering.jl")
+    package_path = raw"D:\development\psrclustering\PSRClustering.jl"
+    assets_path = joinpath(package_path, "compile", "assets")
 
-    ENV["XPRESSDIR"] = ""
-    ENV["XPAUTH_PATH"] = ""
-    ENV["XPRESS_JL_NO_DEPS_ERROR"] = 1
-    ENV["XPRESS_JL_NO_AUTO_INIT"] = 1
-    ENV["XPRESS_JL_SKIP_LIB_CHECK"] = 1
+    configuration = PSRContinuousDeployment.Configuration(
+        "PSRClustering",
+        package_path,
+    )
 
-    PSRContinuousDeployment.compile(configuration)
+    # ENV["XPRESSDIR"] = ""
+    # ENV["XPAUTH_PATH"] = ""
+    # ENV["XPRESS_JL_NO_DEPS_ERROR"] = 1
+    # ENV["XPRESS_JL_NO_AUTO_INIT"] = 1
+    # ENV["XPRESS_JL_SKIP_LIB_CHECK"] = 1
 
-    # if Sys.iswindows()
-    #     PSRContinuousDeployment.create_setup(configuration, "2FE8D94F-A7F8-4CC3-B62C-AD4086F803F3")
+    # PSRContinuousDeployment.compile(
+    #     configuration,
+    #     windows_additional_files_path = [
+    #         joinpath(assets_path, "PSRClustering.bat"),
+    #         joinpath(assets_path, "PSRClustering-pause.bat"),
+    #     ],
+    #     linux_additional_files_path = [
+    #         joinpath(assets_path, "PSRClustering.sh"),
+    #     ],
+    # )
 
-    #     aws_access_key = ENV["AWS_ACCESS_KEY_ID"]
-    #     aws_secret_key = ENV["AWS_SECRET_ACCESS_KEY"]
-    #     PSRContinuousDeployment.deploy_to_psrmodules(configuration, aws_access_key, aws_secret_key)
-
-    #     PSRContinuousDeployment.deploy_to_distribution(configuration, raw"https://bitbucket.org/psr/psrclustering-distribution.git")
-    # end
+    if Sys.iswindows()
+        PSRContinuousDeployment.create_setup(configuration, "2FE8D94F-A7F8-4CC3-B62C-AD4086F803F3")
+    end
 
     return nothing
 end
