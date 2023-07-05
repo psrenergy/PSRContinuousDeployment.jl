39944307974a59363ed08b551249fc8adec792d1
raphasampaio <rsampaio@psr-inc.com>
2023-07-05 17:28:40 -0300

    Minor changes

--- src/release.jl
+++ src/release.jl
@@ -1,20 +1,22 @@
 # https://docs.github.com/pt/rest/releases/releases?apiVersion=2022-11-28#create-a-release
 function create_release(configuration::Configuration, github_key::AbstractString)
-    target = configuration.target
-
     headers = [
         "Accept" => "application/vnd.github+json", 
         "Authorization" => "Bearer $(github_key)",
-        "X-GitHub-Api-Version" => "2022-11-28"]
+        "X-GitHub-Api-Version" => "2022-11-28",
+    ]
+
     data = Dict(
         "tag_name" => "v$(configuration.version)",
         "generate-release-notes" => true,
     )
+
+    target = configuration.target
     response = HTTP.post("https://api.github.com/repos/psrenergy/$(target).jl/releases", headers, JSON.json(data))
+    
     if response.status == 201
-        PSRLogger.info("Release created successfully")
+        PSRLogger.info("GITHUB: Release created successfully")
     else
-        PSRLogger.error("Error creating release")
-        PSRLogger.error(response)
+        PSRLogger.fatal_error("GITHUB: Failed to create release")
     end
 end
