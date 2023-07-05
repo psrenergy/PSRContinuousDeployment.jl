b9a9fafc378c5dff0979c7f90b287fa7ffb1e5c6
pedroripper <pedros.ripper@gmail.com>
2023-07-05 17:48:48 -0300

    Add `check_release_version`

--- src/release.jl
+++ src/release.jl
@@ -20,3 +20,30 @@ function create_release(configuration::Configuration, github_key::AbstractString
         PSRLogger.fatal_error("GITHUB: Failed to create release")
     end
 end
+
+
+function check_release_version(configuration::Configuration, tag_name::String, github_key::AbstractString)
+    tag_name = tag_name[1] == 'v' ? tag_name : "v$tag_name"
+    
+    headers = [
+        "Accept" => "application/vnd.github+json", 
+        "Authorization" => "Bearer $(github_key)",
+        "X-GitHub-Api-Version" => "2022-11-28"]
+
+    target = configuration.target
+    response = HTTP.get("https://api.github.com/repos/psrenergy/$(target).jl/releases", headers)
+
+    if response.status == 200
+        re = r"\"tag_name\":\"(v[0-9]+\.[0-9]+\.[0-9]+)\""
+        m = match(re, String(response))
+        version_tag = String(m[1])
+    else
+        PSRLogger.fatal_error("Error checking version")
+        PSRLogger.fatal_error(response)
+    end
+    if version_tag == tag_name
+        return true
+    else
+        return false
+    end
+end
