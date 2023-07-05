590a2429b37f3287605d26c32d39711a8be94baa
pedroripper <pedros.ripper@gmail.com>
2023-07-05 13:32:49 -0300

    Add release code

--- src/release.jl
+++ src/release.jl
@@ -1,10 +1,18 @@
 # https://docs.github.com/pt/rest/releases/releases?apiVersion=2022-11-28#create-a-release
 function create_release(configuration::Configuration, github_key::AbstractString)
-    headers = []
-    data = [
+    headers = [
         "Accept" => "application/vnd.github+json", 
-        "Authorization" => "Bearer $(github_key))",
+        "Authorization" => "Bearer $(github_key)",
         "X-GitHub-Api-Version" => "2022-11-28"]
-    body = HTTP.Form(data)
-    response = HTTP.post(configuration.package_repo_api_url, headers, body)
+    data = Dict(
+        "tag_name" => "v$(configuration.version)",
+        "generate-release-notes" => true,
+    )
+    response = HTTP.post(configuration.package_repo_api_url, headers, JSON.json(data))
+    if response.status == 201
+        PSRLogger.info("Release created successfully")
+    else
+        PSRLogger.error("Error creating release")
+        PSRLogger.error(response)
+    end
 end
