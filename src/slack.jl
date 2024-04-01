cb66b58cde4f9bbfd1242e3507172b5157ffe5ce
guilhermebodin <guilherme.b.moraes@gmail.com>
2024-04-01 15:27:28 -0300

    treat response status differente than 200.

--- src/slack.jl
+++ src/slack.jl
@@ -5,7 +5,11 @@ function notify_slack_channel(configuration::Configuration, slack_token::Abstrac
     message = "Version $version has been published: https://psr-update-modules.psr-inc.com/$target/$version.zip"
 
     context = SlackContext(slack_token)
-    SlackAPI.channel_message(context, channel, message)
+    response = SlackAPI.channel_message(context, channel, message)
+    if response.status != 200
+        Log.warn("NOTIFY: Failed to notify the Slack channel of code $channel. Status: $(response.status)")
+        return nothing
+    end
 
     return nothing
 end
