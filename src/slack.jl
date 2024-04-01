543723799bc39b2ac44f47b6b1efe225dc674e9a
guilhermebodin <guilherme.b.moraes@gmail.com>
2024-04-01 14:40:06 -0300

    Add Log to show which channel was notified

--- src/slack.jl
+++ src/slack.jl
@@ -1,4 +1,5 @@
 function notify_slack_channel(configuration::Configuration, slack_token::AbstractString, channel::AbstractString)
+    Log.info("NOTIFY: Notifying the Slack channel of code $channel")
     target = configuration.target
     version = configuration.version
     message = "Version $version has been published: https://psr-update-modules.psr-inc.com/$target/$version.zip"
