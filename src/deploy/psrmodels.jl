45684c6ba7a28d7ebf3afd845f2c3bdce810cd63
raphasampaio <raphaelaraujosampaio@gmail.com>
2024-12-05 19:40:03 -0300

    Updated

---
+++ src/deploy/psrmodels.jl
@@ -0,0 +1,83 @@
+function generate_unique_key(; bucket::AbstractString, version::AbstractString, target::AbstractString, setup_zip::AbstractString, overwrite::Bool = false)
+    objects = S3.list_objects_v2(bucket, Dict("prefix" => target))
+    if haskey(objects, "Contents")
+        for contents in objects["Contents"]
+            key = contents["Key"]
+            if startswith(key, "$target/$version/")
+                if overwrite
+                    Log.info("PSRMODELS: Overwriting the $setup_zip in the $bucket bucket")
+                    return key
+                else
+                    Log.fatal_error("PSRMODELS: The $setup_zip already exists in the $bucket bucket")
+                end
+            end
+        end
+    end
+
+    for _ in 1:10
+        hash = randstring(['a':'z'; '0':'9'], 8)
+        key = "$target/$version/$hash/$setup_zip"
+
+        objects = S3.list_objects_v2(bucket, Dict("prefix" => target))
+        if haskey(objects, "Contents")
+            unique = true
+            for contents in objects["Contents"]
+                if key == contents["Key"]
+                    unique = false
+                    break
+                end
+            end
+
+            if unique
+                Log.info("PSRMODELS: Generated a unique key: $key")
+                return key
+            end
+        else
+            return key
+        end
+    end
+
+    error("Failed to generate a unique hash")
+end
+
+function deploy_to_psrmodels(;
+    configuration::Configuration,
+    aws_access_key::AbstractString,
+    aws_secret_key::AbstractString,
+    overwrite::Bool = false,
+)
+    bucket = "psr-models"
+
+    target = configuration.target
+    version = configuration.version
+    setup_path = configuration.setup_path
+
+    setup_exe = "$target-$version-setup.exe"
+    setup_exe_path = joinpath(setup_path, setup_exe)
+
+    setup_zip = "$target-$version.zip"
+    setup_zip_path = joinpath(setup_path, setup_zip)
+
+    Log.info("PSRMODELS: Zipping $setup_exe")
+    run(`$(p7zip_jll.p7zip()) a -tzip $setup_zip_path $setup_exe_path`)
+    @assert isfile(setup_zip_path)
+
+    aws_credentials = AWSCredentials(aws_access_key, aws_secret_key)
+    aws_config = AWSConfig(; creds = aws_credentials, region = "us-east-1")
+    global_aws_config(aws_config)
+
+    key = generate_unique_key(
+        bucket = bucket,
+        version = version,
+        target = target,
+        setup_zip = setup_zip,
+        overwrite = overwrite,
+    )
+
+    Log.info("PSRMODELS: Uploading $setup_zip")
+    S3.put_object(bucket, key, Dict("body" => read(setup_zip_path)))
+
+    Log.info("PSRMODELS: Success")
+
+    return "https://$bucket.psr-inc.com/$key"
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,12 @@ using PSRContinuousDeployment
 
 using Test
 
+const SLACK_CHANNEL = "C03SSPFNTJS"
+
+const AWS_ACCESS = ENV["AWS_ACCESS_KEY_ID"]
+const AWS_SECRET = ENV["AWS_SECRET_ACCESS_KEY"]
+const SLACK_TOKEN = ENV["SLACK_BOT_USER_OAUTH_ACCESS_TOKEN"]
+
 function testall()
     package_path = joinpath(@__DIR__, "Example.jl")
     database_path = joinpath(package_path, "database")
@@ -20,6 +26,25 @@ function testall()
             joinpath(package_path, "Example.bat"),
         ],
         skip_version_jl = true,
+    )
+
+    create_setup(
+        configuration,
+        sign = false,
+    )
+
+    url = deploy_to_psrmodels(
+        configuration = configuration,
+        aws_access_key = AWS_ACCESS,
+        aws_secret_key = AWS_SECRET,
+        overwrite = true,
+    )
+
+    notify_slack_channel(
+        configuration = configuration,
+        slack_token = SLACK_TOKEN,
+        channel = SLACK_CHANNEL,
+        url = url,
     )
 
     return 0
