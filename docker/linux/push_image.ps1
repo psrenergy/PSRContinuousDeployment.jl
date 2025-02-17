7b1c2ce1d374a4f105e2bf3a7a38bb6d03151c63
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-02-17 15:03:59 -0300

    Updated

---
+++ docker/linux/push_image.ps1
@@ -0,0 +1,3 @@
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 747020361772.dkr.ecr.us-east-1.amazonaws.com
+docker build -t 747020361772.dkr.ecr.us-east-1.amazonaws.com/julia_publish:latest .
+docker push 747020361772.dkr.ecr.us-east-1.amazonaws.com/julia_publish:latest
