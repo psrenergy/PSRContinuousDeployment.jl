811e65c30da404441fdb5ffb5aecb109fcea4bb1
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-03-18 13:58:59 -0300

    [skip ci] minor changes

---
+++ docker/windows/entrypoint.ps1
@@ -0,0 +1,22 @@
+# clone repo
+$repository = ($env:GITHUB_REPOSITORY -split "/"| Select-Object -Last 1) -replace ".git$", ""
+git clone -n ($env:GITHUB_REPOSITORY.Substring(0,8) + "psrcloud:" + "$env:PERSONAL_ACCESS_TOKEN@" + $env:GITHUB_REPOSITORY.Substring(8))
+Set-Location $repository
+git checkout $env:GITHUB_SHA
+
+# get the julia version
+if ((Get-Content Manifest.toml -Raw) -match 'julia_version\s*=\s*"([^"]+)"') { $JULIA_VERSION = $Matches[1] }
+
+# setup julia
+$JULIA_VERSION_SHORT = ($JULIA_VERSION -split "\.")[0,1] -join "."
+$JULIA_VERSION_ENV = $JULIA_VERSION -replace "\.", ""
+$ProgressPreference = "SilentlyContinue"
+Invoke-WebRequest -Uri "https://julialang-s3.julialang.org/bin/winnt/x64/$JULIA_VERSION_SHORT/julia-$JULIA_VERSION-win64.zip" 
+-OutFile julia-$JULIA_VERSION-win64.zip -UseBasicParsing
+Expand-Archive -Path julia-$JULIA_VERSION-win64.zip -DestinationPath .
+Set-Item env:JULIA_$JULIA_VERSION_ENV "$((Get-Location).Path)\julia-$($JULIA_VERSION)\bin\julia.exe"
+
+# compile and publish
+$env:JULIA_PKG_USE_CLI_GIT=$true
+.\compile\compile.bat --development_stage $DEVELOPMENT_STAGE --version_suffix $VERSION_SUFFIX
+.\compile\publish.bat --development_stage $DEVELOPMENT_STAGE --version_suffix $VERSION_SUFFIX --overwrite $OVERWRITE
