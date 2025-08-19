b592825306cc156dd5726c114e9975b4ca579103
raphasampaio <raphaelaraujosampaio@gmail.com>
2025-08-19 09:31:00 -0300

    Updated

--- docker/windows/push_image.ps1
+++
@@ -1,3 +0,0 @@
-aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 747020361772.dkr.ecr.us-east-1.amazonaws.com
-docker build -t 747020361772.dkr.ecr.us-east-1.amazonaws.com/julia-publish-windows:latest .
-docker push 747020361772.dkr.ecr.us-east-1.amazonaws.com/julia-publish-windows:latest
